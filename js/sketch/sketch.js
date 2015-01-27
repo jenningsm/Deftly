7fa593d888be0b994e04c18a600feb578fda1aa6
Michael Jennings <mpjngs@gmail.com>
2015-01-26 16:06:37 -0800

    small change

--- js/sketch/sketch.js
+++ js/sketch/sketch.js
@@ -20,17 +20,8 @@ function setup(){
     var radWidth = 250 / (1 + dist);
     //radWidth *= radWidth * dist / (rad + (radWidth / 2) + dist);
     var rand = Math.random();
-    var colors;
-    if(rand > .2){
-      colors = [Math.random() * 64, Math.random() * 32, 64 + Math.random() * 196, 128];
-    } else if(rand > .1) {
-      var shade = Math.random() * 96 + 32;
-      colors = [shade, shade, shade, 128];
-    } else {
-      colors = [255, 96, 0, 128];
-    }
     var shade = Math.random() * 224 + 32;
-    colors = [shade, shade, shade, 160];
+    var colors = [shade, shade, shade, 160];
     arcs.push(new Arc(Math.PI / 4 + Math.random() - .5, Math.random() * 2 * PI, radWidth, rad, .002 + (Math.random() - .5) / 25, colors));
     arcs[i].draw();
   }

30d7da1127790d5c934ba35001691d19e9b06819
Michael Jennings <mpjngs@gmail.com>
2015-02-24 19:45:34 -0800

    got new mover built

--- js/misc/util.js
+++ js/misc/util.js
@@ -1,4 +1,10 @@
 
+window.requestAnimationFrame = window.requestAnimationFrame
+ || window.mozRequestAnimationFrame
+ || window.webkitRequestAnimationFrame
+ || window.msRequestAnimationFrame
+ || function(f){setTimeout(f, 1000/60)};
+
 function sequence(functions){
   if(functions.length != 0){
     functions[0](partial(sequence, functions.slice(1)));
--- js/mover.js
+++ js/mover.js
@@ -1,27 +1,32 @@
 
-function mover(shift){
+function mover(startSpeed, stopSpeed, accel, decel, maxSpeed){
 
-
-  return function(from, to, scale, startSpeed, stopSpeed, maxSpeed, accel, decel, next){
-
-    //the point in time at which the item will begin to decelerate
-    var decelAt = Math.max((decel + stopSpeed - startSpeed) / (decel + accel), (maxSpeed - stopSpeed) / decel);
-    
-    //the point in time at which the item will stop accelerating
-    var stopAccelAt = Math.min(decelAt, (maxSpeed - startSpeed) / accel);
-
-    function m(x){
-      //amount of time spent accelerating, floating, and decelerating, respectively
-      var accelFor = Math.min(x, stopAccelAt);
-      var floatFor = Math.max(0, Math.min(x - stopAccelAt, decelAt - stopAccelAt));
-      var decelFor = Math.max(0, x - decelAt);
-
-      //amount of distance traveled while accelerating, floating, and decelerating, respectively
-      var accelDist = .5 * accel * accelFor * accelFor + accelFor * startSpeed;
-      var floatDist = floatFor * maxSpeed;
-      var decelDist = -.5 * decel * decelFor * decelFor + decelFor * (stopSpeed + decel * (1 - x));
-
-      shift(accelDist + floatDist + decelDist);
-    } 
+  //the point in time at which the item will begin to decelerate
+  var decelAt = (decel + stopSpeed - startSpeed) / (decel + accel);
+  if(maxSpeed !== 0){
+    decelAt = Math.max(decelAt, (maxSpeed - stopSpeed) / decel);
   }
+  
+  //the point in time at which the item will stop accelerating
+  var stopAccelAt = Math.min(decelAt, (maxSpeed - startSpeed) / accel);
+
+  //the total distance that will be travelled, used to scale down to 1
+  var totalDist = 0;
+  totalDist += .5 * accel * Math.pow(stopAccelAt, 2);
+  totalDist += maxSpeed * (decelAt - stopAccelAt);
+  totalDist += -.5 * decel * Math.pow(decelfor, 2) + decelFor * stopSpeed;
+
+  return function(x){
+    //amount of time spent accelerating, floating, and decelerating, respectively
+    var accelFor = Math.min(x, stopAccelAt);
+    var floatFor = Math.max(0, Math.min(x - stopAccelAt, decelAt - stopAccelAt));
+    var decelFor = Math.max(0, x - decelAt);
+
+    //amount of distance traveled while accelerating, floating, and decelerating, respectively
+    var accelDist = .5 * accel * accelFor * accelFor + accelFor * startSpeed;
+    var floatDist = floatFor * maxSpeed;
+    var decelDist = -.5 * decel * decelFor * decelFor + decelFor * (stopSpeed + decel * (1 - x));
+
+    return (accelDist + floatDist + decelDist) / totalDist;
+  } 
 }

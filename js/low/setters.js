4a7ffe6296da2f7e67086c6082cf9471d9347feb
Michael Jennings <mpjngs@gmail.com>
2015-03-12 16:58:29 -0700

    fixed bug where clipped items weren't showing on resize

--- js/low/setters.js
+++ js/low/setters.js
@@ -1,25 +1,46 @@
 
-function displayHandler(element){
+function displayHandler(){
   var last = -1;
 
   return function(x){
+    var display = null;
     if(last !== 0 && x === 0){
-      element.style.display = 'none';
+      display = 'none';
     } else if((last === 0 || last === -1) && x !== 0){
-      element.style.display = 'block';
+      display = 'block';
     }
     last = x;
+    return display;
   }
 }
 
 function clip(element, orientation){
-  var width = element.offsetWidth;
-  var height = element.offsetHeight;
   var currentClip = null;
+  var resized = false;
+  var display = null;
+  var d = displayHandler();
 
-  var d = displayHandler(element);
+  var width;
+  var height;
+  function getDimensions(){
+    width = element.offsetWidth;
+    height = element.offsetHeight;
+  }
+  getDimensions();
+
 
   function clipit(x) {
+
+     var a = d(x);
+     if(a !== null){
+       display = a;
+       element.style.display = a;
+       if(a !== 'none' && resized){
+         getDimensions();
+         resized = false;
+       }
+     }
+
      currentClip = x;
      var left = (orientation ? width * (1 - x) / 2 : 0);
      var right = (orientation ? width * x + left : width);
@@ -27,15 +48,17 @@ function clip(element, orientation){
      var bottom = (orientation ? height : height * x + tp);
      element.style.clip = "rect(" + tp + "px, " + right + "px, " + bottom + "px, " + left + ")";
 
-     d(x);
   }
+
 
   function onResize(){
-    width = element.offsetWidth;
-    height = element.offsetHeight;
-    clipit(currentClip);
+    if(display === 'none'){
+      resized = true;
+    } else {
+      getDimensions();
+      clipit(currentClip);
+    }
   }
-
   window.addEventListener('resize', onResize);
 
   return clipit;
@@ -43,27 +66,36 @@ function clip(element, orientation){
 }
 
 function opacity(element, display){
-  var d = displayHandler(element);
+  var d = displayHandler();
   return function(x){
     element.style.opacity = x;
-    d(x);
+    var a = d(x);
+    if(a !== null){
+      element.style.display = a;
+    }
   }
 }
 
 function scale(element, orientation){
-  var d = displayHandler(element);
+  var d = displayHandler();
   return function(x) {
      element.style.transform = "scale3d(" + (orientation ? x : 1) + ", " + (orientation ? 1 : x) + ", 1)";
-     d(x);
+     var a = d(x);
+     if(a !== null){
+       element.style.display = a;
+     }
   }
 }
 
 function shift(element, shiftdir, orientation){
-   var d = displayHandler(element);
+   var d = displayHandler();
    return function(x) {
      var shift = (((1 - x) * 100) * (shiftdir ? -1 : 1));
      element.style.transform = "translate3d(" + (orientation ? "0%" : shift + "%") + ", " + (orientation ? shift + "%" : "0%") + ", 0)";
-     d(x);
+     var a = d(x);
+     if(a !== null){
+       element.style.display = a;
+     }
    }
 }
 

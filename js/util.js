b3549f3d1b5e01e76d62abc07ca59f1ee8269a51
Michael Jennings <mpjngs@gmail.com>
2015-03-11 18:21:50 -0700

    change + documentation to for sequence

--- js/util.js
+++ js/util.js
@@ -26,17 +26,30 @@ function validFunc(func){
   return (func !== null && func !== undefined);
 }
 
-function riv(func, next){
-  if(validFunc(func))
-    func(next);
-  else if(validFunc(next))
-    next();
+function riv(func){
+  if(validFunc(func)){
+    func();
+  }
 }
+
+/*
+  Takes an array of functions to be executed in order. If any of the functions
+   is interrupted while that function is executing, the remaining functions will not be
+   executed, but chaincb will be called. If all the functions finish executing without
+   interruption, chaincb will be called at the end.
+
+   each function in functions takes one argument, which is a callback to sequence.
+
+   If a function calls the callback with an argument of 1, this alerts sequence that
+   that function has been interrupted. If a function does not ever get interrupted it
+   will call the callback with an agrument of 0 or no argument at all once it has
+   finished executing. 
+*/
 
 function sequence(functions, chaincb, s){
   if(functions.length !== 0 && validFunc(functions[0]) && s !== 1){
       functions[0](partial(sequence, functions.slice(1), chaincb));
   } else if(validFunc(chaincb)){
-      chaincb(0);
+      chaincb();
   }
 }
